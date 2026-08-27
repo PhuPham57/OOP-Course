7ae57abd2a74aec1a174bd95238a5df528554bf7
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-08-27 23:45:43 +0700

    Update

---
+++ src/week2/Homework.cpp
@@ -0,0 +1,260 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // STUDENT 1
+    string studentName1 = "Nguyen Van A";
+    string studentID1 = "23110001";
+    string studentPhone1 = "0901234567";
+
+    // STUDENT 2
+    string studentName2 = "Tran Thi B";
+    string studentID2 = "23110002";
+    string studentPhone2 = "0912345678";
+
+    // STUDENT 3
+    string studentName3 = "Le Van C";
+    string studentID3 = "23110003";
+    string studentPhone3 = "0923456789";
+
+    // STUDENT 4
+    string studentName4 = "Pham Thi D";
+    string studentID4 = "23110004";
+    string studentPhone4 = "0934567890";
+
+    // STUDENT 5
+    string studentName5 = "Hoang Van E";
+    string studentID5 = "23110005";
+    string studentPhone5 = "0945678901";
+
+    // STUDENT 6
+    string studentName6 = "Vo Thi F";
+    string studentID6 = "23110006";
+    string studentPhone6 = "0956789012";
+
+    // STUDENT 7
+    string studentName7 = "Dang Van G";
+    string studentID7 = "23110007";
+    string studentPhone7 = "0967890123";
+
+    // STUDENT 8
+    string studentName8 = "Bui Thi H";
+    string studentID8 = "23110008";
+    string studentPhone8 = "0978901234";
+
+    // STUDENT 9
+    string studentName9 = "Do Van I";
+    string studentID9 = "23110009";
+    string studentPhone9 = "0989012345";
+
+    // STUDENT 10
+    string studentName10 = "Nguyen Thi K";
+    string studentID10 = "23110010";
+    string studentPhone10 = "0901123456";
+
+    // STUDENT 11
+    string studentName11 = "Tran Van L";
+    string studentID11 = "23110011";
+    string studentPhone11 = "0912234567";
+
+    // STUDENT 12
+    string studentName12 = "Le Thi M";
+    string studentID12 = "23110012";
+    string studentPhone12 = "0923345678";
+
+    // STUDENT 13
+    string studentName13 = "Pham Van N";
+    string studentID13 = "23110013";
+    string studentPhone13 = "0934456789";
+
+    // STUDENT 14
+    string studentName14 = "Hoang Thi O";
+    string studentID14 = "23110014";
+    string studentPhone14 = "0945567890";
+
+    // STUDENT 15
+    string studentName15 = "Vo Van P";
+    string studentID15 = "23110015";
+    string studentPhone15 = "0956678901";
+
+    // STUDENT 16
+    string studentName16 = "Dang Thi Q";
+    string studentID16 = "23110016";
+    string studentPhone16 = "0967789012";
+
+    // STUDENT 17
+    string studentName17 = "Bui Van R";
+    string studentID17 = "23110017";
+    string studentPhone17 = "0978890123";
+
+    // STUDENT 18
+    string studentName18 = "Do Thi S";
+    string studentID18 = "23110018";
+    string studentPhone18 = "0989901234";
+
+    // STUDENT 19
+    string studentName19 = "Nguyen Van T";
+    string studentID19 = "23110019";
+    string studentPhone19 = "0901012345";
+
+    // STUDENT 20
+    string studentName20 = "Le Van U";
+    string studentID20 = "23110020";
+    string studentPhone20 = "0987654321";
+
+
+    cout << "==============================================" << endl;
+    cout << "                 STUDENT LIST                 " << endl;
+    cout << "==============================================" << endl;
+
+    cout << endl;
+
+    cout << "Student 1" << endl;
+    cout << "Name       : " << studentName1 << endl;
+    cout << "Student ID : " << studentID1 << endl;
+    cout << "Phone      : " << studentPhone1 << endl;
+
+    cout << endl;
+
+    cout << "Student 2" << endl;
+    cout << "Name       : " << studentName2 << endl;
+    cout << "Student ID : " << studentID2 << endl;
+    cout << "Phone      : " << studentPhone2 << endl;
+
+    cout << endl;
+
+    cout << "Student 3" << endl;
+    cout << "Name       : " << studentName3 << endl;
+    cout << "Student ID : " << studentID3 << endl;
+    cout << "Phone      : " << studentPhone3 << endl;
+
+    cout << endl;
+
+    cout << "Student 4" << endl;
+    cout << "Name       : " << studentName4 << endl;
+    cout << "Student ID : " << studentID4 << endl;
+    cout << "Phone      : " << studentPhone4 << endl;
+
+    cout << endl;
+
+    cout << "Student 5" << endl;
+    cout << "Name       : " << studentName5 << endl;
+    cout << "Student ID : " << studentID5 << endl;
+    cout << "Phone      : " << studentPhone5 << endl;
+
+    cout << endl;
+
+    cout << "Student 6" << endl;
+    cout << "Name       : " << studentName6 << endl;
+    cout << "Student ID : " << studentID6 << endl;
+    cout << "Phone      : " << studentPhone6 << endl;
+
+    cout << endl;
+
+    cout << "Student 7" << endl;
+    cout << "Name       : " << studentName7 << endl;
+    cout << "Student ID : " << studentID7 << endl;
+    cout << "Phone      : " << studentPhone7 << endl;
+
+    cout << endl;
+
+    cout << "Student 8" << endl;
+    cout << "Name       : " << studentName8 << endl;
+    cout << "Student ID : " << studentID8 << endl;
+    cout << "Phone      : " << studentPhone8 << endl;
+
+    cout << endl;
+
+    cout << "Student 9" << endl;
+    cout << "Name       : " << studentName9 << endl;
+    cout << "Student ID : " << studentID9 << endl;
+    cout << "Phone      : " << studentPhone9 << endl;
+
+    cout << endl;
+
+    cout << "Student 10" << endl;
+    cout << "Name       : " << studentName10 << endl;
+    cout << "Student ID : " << studentID10 << endl;
+    cout << "Phone      : " << studentPhone10 << endl;
+
+    cout << endl;
+
+    cout << "Student 11" << endl;
+    cout << "Name       : " << studentName11 << endl;
+    cout << "Student ID : " << studentID11 << endl;
+    cout << "Phone      : " << studentPhone11 << endl;
+
+    cout << endl;
+
+    cout << "Student 12" << endl;
+    cout << "Name       : " << studentName12 << endl;
+    cout << "Student ID : " << studentID12 << endl;
+    cout << "Phone      : " << studentPhone12 << endl;
+
+    cout << endl;
+
+    cout << "Student 13" << endl;
+    cout << "Name       : " << studentName13 << endl;
+    cout << "Student ID : " << studentID13 << endl;
+    cout << "Phone      : " << studentPhone13 << endl;
+
+    cout << endl;
+
+    cout << "Student 14" << endl;
+    cout << "Name       : " << studentName14 << endl;
+    cout << "Student ID : " << studentID14 << endl;
+    cout << "Phone      : " << studentPhone14 << endl;
+
+    cout << endl;
+
+    cout << "Student 15" << endl;
+    cout << "Name       : " << studentName15 << endl;
+    cout << "Student ID : " << studentID15 << endl;
+    cout << "Phone      : " << studentPhone15 << endl;
+
+    cout << endl;
+
+    cout << "Student 16" << endl;
+    cout << "Name       : " << studentName16 << endl;
+    cout << "Student ID : " << studentID16 << endl;
+    cout << "Phone      : " << studentPhone16 << endl;
+
+    cout << endl;
+
+    cout << "Student 17" << endl;
+    cout << "Name       : " << studentName17 << endl;
+    cout << "Student ID : " << studentID17 << endl;
+    cout << "Phone      : " << studentPhone17 << endl;
+
+    cout << endl;
+
+    cout << "Student 18" << endl;
+    cout << "Name       : " << studentName18 << endl;
+    cout << "Student ID : " << studentID18 << endl;
+    cout << "Phone      : " << studentPhone18 << endl;
+
+    cout << endl;
+
+    cout << "Student 19" << endl;
+    cout << "Name       : " << studentName19 << endl;
+    cout << "Student ID : " << studentID19 << endl;
+    cout << "Phone      : " << studentPhone19 << endl;
+
+    cout << endl;
+
+    cout << "Student 20" << endl;
+    cout << "Name       : " << studentName20 << endl;
+    cout << "Student ID : " << studentID20 << endl;
+    cout << "Phone      : " << studentPhone20 << endl;
+
+    cout << endl;
+
+    cout << "==============================================" << endl;
+    cout << "Total students: 20" << endl;
+    cout << "==============================================" << endl;
+
+    return 0;
+}
