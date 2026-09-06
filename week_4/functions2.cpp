28c02940e0b303b311b0ff5abf578c0f2429a2a6
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-06 23:57:04 +0700

    Update functions2.cpp

--- week_4/functions2.cpp
+++ week_4/functions2.cpp
@@ -5,11 +5,16 @@ int n = 0;
 string names[20];
 string ids[20];
 string phones[20]; 
-void inputStudent(){
+
+void inputStudent()
+{
     cout << "Input the number of students: ";
     cin >> n; 
-    if (n>0){
-        for (int i = 0; i<n;i++){
+
+    if (n > 0)
+    {
+        for (int i = 0; i<n;i++)
+        {
             cout << "Enter information of student " << i + 1 << "\n";
             cout << "Name: ";
             getline(cin,names[i]);
@@ -21,22 +26,26 @@ void inputStudent(){
         cout << "Number of inputted students: " << n;
 
     }
-    else{
+
+    else
+    {
         cout << "The number of students must be more than 0";
     }
 }
 
-void outputStudents(){
-    for (int i = 0; i<n;i++){
+void outputStudents()
+{
+    for (int i = 0; i<n;i++)
+    {
         cout << "Student: " << i + 1 << "\n";
         cout << "Name: " << names[i] << "\n";
         cout << "ID: " << ids[i] << "\n";
         cout << "Phone: " << phones[i] << "\n";
     }
 }
+
 int main()
 {
-    std::cout << "Hello, C++!" << std::endl;
     inputStudent();
     cout << n;
     outputStudents();
