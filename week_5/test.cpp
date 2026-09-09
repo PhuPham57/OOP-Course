1cab89fa0b16b20cdefcd4ca0345e66e6be55e6e
PhuPham57 <fdpminecraftgamin@gmail.com>
2026-09-09 10:28:49 +0700

    bo

--- week_5/test.cpp
+++ week_5/test.cpp
@@ -87,56 +87,8 @@ int main()
             cout << "Yeu\n";
         }
     }
-
-    cout << "\nSo sanh hai hoc sinh ke tiep:\n";
-    for (int i = 0; i < N - 1; i++) {
-        if (scores[i + 1] > scores[i]) {
-            cout << "  + Hoc sinh " << i + 2 << " tien bo hon Hoc sinh " << i + 1 << "\n";
-        }
-    }
-
-    int maxStreak = 0;
-    int currentStreak = 0;
-    for (int i = 0; i < N; i++) {
-        if (scores[i] >= 5.0) {
-            currentStreak++;
-            if (currentStreak > maxStreak) maxStreak = currentStreak;
-        } else {
-            currentStreak = 0;
-        }
-    }
-    cout << "\nChuoi dau lien tuc dai nhat: " << maxStreak << " hoc sinh\n";
-
-
-    cout << "\nPhan bo diem so:\n";
-    int c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0;
-    for (int i = 0; i < N; i++) {
-        if (scores[i] < 5.0) c1++;
-        else if (scores[i] < 6.5) c2++;
-        else if (scores[i] < 8.0) c3++;
-        else if (scores[i] < 9.0) c4++;
-        else c5++;
-    }
-
-    cout << "0   - <5  : ";
-    for (int i = 0; i < c1; i++) cout << "*";
-    cout << " " << c1 << "\n";
-
-    cout << "5   - <6.5: ";
-    for (int i = 0; i < c2; i++) cout << "*";
-    cout << " " << c2 << "\n";
-
-    cout << "6.5 - <8  : ";
-    for (int i = 0; i < c3; i++) cout << "*";
-    cout << " " << c3 << "\n";
-
-    cout << "8   - <9  : ";
-    for (int i = 0; i < c4; i++) cout << "*";
-    cout << " " << c4 << "\n";
-
-    cout << "9   - 10  : ";
-    for (int i = 0; i < c5; i++) cout << "*";
-    cout << " " << c5 << "\n";
-
+    
+    
+    
     return 0;
 }
