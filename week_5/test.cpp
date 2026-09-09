7f5ade3d38ad8b2d9d08bbf6d38bf0a0ba07df8f
PhuPham57 <fdpminecraftgamin@gmail.com>
2026-09-09 09:13:17 +0700

    buoc 1

--- week_5/test.cpp
+++ week_5/test.cpp
@@ -4,5 +4,17 @@ using namespace std;
 
 int main()
 {
+    int N;
+
+    do {
+        cout << "Nhap so luong hoc sinh: ";
+        cin >> N;
+        if (N < 2 || N > 20) {
+            cout << "So luong khong hop le! Vui long nhap tu 2 den 20.\n";
+        }
+    } while (N < 2 || N > 20);
+
+    double scores[20];
+    
     return 0;
 }
