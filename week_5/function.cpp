40d35f7841b5f42e00214f028a5d8174573ed5bb
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-11 10:21:43 +0700

    step 5

--- week_5/function.cpp
+++ week_5/function.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+struct ScoreInfo {
+    double sum;
+    double average;
+    int count;
+    double maxScore;
+    double minScore;
+};
+
 // Function definition
 void showMenu() {
     cout << "===== MENU =====" << endl;
@@ -25,6 +33,32 @@ int maxValue(int a, int b) {
     } else {
         return b;
     }
+}
+
+ScoreInfo analyzeScores(const double scores[], int size) {
+    ScoreInfo result;
+    result.sum = 0;
+    result.count = size;
+    
+    if (result.count == 0) {
+        result.average = 0;
+        result.maxScore = 0;
+        result.minScore = 0;
+        return result;
+    }
+    
+    result.minScore = scores[0];
+    result.maxScore = scores[0];
+    
+    for (int i = 0; i < size; ++i) {
+        double s = scores[i];
+        result.sum += s;
+        if (s > result.maxScore) result.maxScore = s;
+        if (s < result.minScore) result.minScore = s;
+    }
+    
+    result.average = result.sum / result.count;
+    return result;
 }
 
 int main() {
@@ -62,6 +96,16 @@ int main() {
     int largest = maxValue(num1, num2);
     
     cout << "The larger value is: " << largest << endl;
+
+    double scores[] = {8.5, 7.0, 9.5, 6.0, 8.0};
+    int size = sizeof(scores) / sizeof(scores[0]);
+    
+    ScoreInfo info = analyzeScores(scores, size);
+    
+    cout << "Total: " << info.sum << endl;
+    cout << "Average: " << info.average << endl;
+    cout << "Max score: " << info.maxScore << endl;
+    cout << "Min score: " << info.minScore << endl;
 
     return 0;
 }
