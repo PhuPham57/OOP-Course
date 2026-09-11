578dd0386123ea46448a60621e862c974e9bcdbd
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-11 10:16:08 +0700

    step 2

--- week_5/function.cpp
+++ week_5/function.cpp
@@ -9,11 +9,23 @@ void showMenu() {
     cout << "3. Exit" << endl;
 }
 
+void showStudentID(int studentID) {
+    cout << "Student ID: " << studentID << endl;
+}
+
 int main() {
     // Call the function
     showMenu();
     
     cout << endl;
     cout << "Let's get started!" << endl;
+
+    int studentID;
+    cout << "Enter your student ID: ";
+    cin >> studentID;
+    
+    // Call the function
+    showStudentID(studentID);
+    
     return 0;
 }
