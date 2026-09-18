d8c65cad7ab1f1d46457b5d4c39040480ed92511
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-18 09:58:30 +0700

    Create reference4.cpp

---
+++ week_8/reference4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void changeValue(int x) {
+    x = 100;    // pass by reference
+}
+
+void changeRef(int& x) {
+    x = 200;    // pass by value
+}
+
+int main() {
+    int a = 5;
+    changeValue(a);
+    cout << "After changeValue: " << a << endl;
+    changeRef(a);
+    cout << "After changeRef: " << a << endl;
+    return 0;
+}
