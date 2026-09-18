82c08c7c02cbf2b7598d1f59b6fff92562c999f0
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-18 09:10:33 +0700

    step 4

--- week_8/prac1.cpp
+++ week_8/prac1.cpp
@@ -14,7 +14,13 @@ int main() {
     cout << "Enter hometown: ";
     getline(cin, hometown);
 
-    
+    cout << "\n===== STUDENT PROFILE =====\n";
+    cout << "Full name  : " << fullName << endl;
+    cout << "Student ID : " << studentId << endl;
+    cout << "Email      : " << email << endl;
+    cout << "Hometown   : " << hometown << endl;
+    cout << "===========================\n";
+
     
     return 0;
 }
