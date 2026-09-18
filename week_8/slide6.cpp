175c8047ad896a986a77aaf04de152bed8eb0833
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-18 09:24:28 +0700

    3

--- week_8/slide6.cpp
+++ week_8/slide6.cpp
@@ -11,4 +11,12 @@ int main() {
     string s2 = "Hi";
     cout << s1.empty() << endl;
     cout << s2.empty() << endl;
+
+    string text = "I love C++";
+    size_t pos = text.find("C++");
+    if (pos != string :: npos) {
+        cout << "Found at: " << pos << endl;
+    } else {
+        cout << "Not found!" << endl;
+    }
 }
