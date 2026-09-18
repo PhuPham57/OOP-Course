6c56c0bafcc9688d5bcf2ed41f0edc44f953f255
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-18 09:10:20 +0700

    step3

--- week_8/prac1.cpp
+++ week_8/prac1.cpp
@@ -5,6 +5,16 @@ using namespace std;
 int main() {
     string fullName, studentId, email, hometown;
 
+    cout << "Enter full name: ";
+    getline(cin, fullName);
+    cout << "Enter student ID: ";
+    getline(cin, studentId);
+    cout << "Enter email: ";
+    getline(cin, email);
+    cout << "Enter hometown: ";
+    getline(cin, hometown);
+
+    
     
     return 0;
 }
