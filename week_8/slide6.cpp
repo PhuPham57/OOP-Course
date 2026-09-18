3be136f81f790d00e3b8f7b0defd2ee1b856a36e
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-18 09:24:04 +0700

    2

--- week_8/slide6.cpp
+++ week_8/slide6.cpp
@@ -6,4 +6,9 @@ int main() {
     string s = "Hello C++";
     cout << s.length() << endl;
     cout << s.size() << endl;
+
+    string s1 = "";
+    string s2 = "Hi";
+    cout << s1.empty() << endl;
+    cout << s2.empty() << endl;
 }
