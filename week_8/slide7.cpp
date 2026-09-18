74843e91651545090e8d09bbf5ce23858279f4f8
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-18 09:31:41 +0700

    4

--- week_8/slide7.cpp
+++ week_8/slide7.cpp
@@ -38,5 +38,16 @@ int main() {
     }
     cout << endl;
 
+    string sentence, word;
+    getline(cin, sentence);
+    getline(cin, word);
+
+    size_t pos = sentence.find(word);
+    if (pos != string::npos) {
+        cout << "The word \"" << word << "\" is found at position " << pos << "." << endl;
+    } else {
+        cout << "The word \"" << word << "\" is not found." << endl;
+    }
+
     return 0;
 }
