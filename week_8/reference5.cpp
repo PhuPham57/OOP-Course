cb7230452ade01070cc98ae7a3044126ca480dd0
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-18 10:00:29 +0700

    Create reference5.cpp

---
+++ week_8/reference5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void setLastToTen(int& x) {
+    x = 10;
+}
+
+int main() {
+    int nums[4] = {3, 5, 7, 9};
+    setLastToTen(nums[3]); // pass last elenent by reference
+    for (int i = 0; i < 4; ++i)
+        cout << nums[i] << " ";
+        cout << endl;
+    return 0;
+}
