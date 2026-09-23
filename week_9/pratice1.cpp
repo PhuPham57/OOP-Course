f2dbb17f36e2c76991e2211d2517763ae3077c71
PhuPham <53972049+PhuPham57@users.noreply.github.com>
2026-09-23 09:01:37 +0700

    2

--- week_9/pratice1.cpp
+++ week_9/pratice1.cpp
@@ -41,13 +41,33 @@ struct Restaurant
         foodCount = 0;
     }
 
-    void addFood(int n)
+    void addFood()
     {
         int n;
         cout << "Them mon an cho quan: \n";
         cout << "Nhap so luong mon an can them: ";
         cin >> n;
-    
+        cin.ignore();
+
+        for(int i = foodCount; i < foodCount + n; i++)
+        {
+            cout << "\nNhap mon thu " << i + 1 << ":\n";
+
+            cout << "Nhap ID mon an: ";
+            getline(cin, foods[i].id);
+
+            cout << "Nhap ten mon an: ";
+            getline(cin, foods[i].name);
+
+            cout << "Nhap gia: ";
+            cin >> foods[i].price;
+
+            cout << "Nhap so luong: ";
+            cin >> foods[i].quantity;
+
+            cin.ignore();
+        }
+        foodCount += n;
     }
 };
 
